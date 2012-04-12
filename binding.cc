a3735089ef81e7406b7794bdc3848965220d8732
Stéphan Kochen <stephan@kochen.nl>
2012-04-12 19:28:00 +0200

    Adding license notices to some source files.

--- binding.cc
+++ binding.cc
@@ -1,7 +1,30 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 #include <string.h>
 #include <v8.h>
 #include <node.h>
 #include <node_buffer.h>
+
 
 using namespace v8;
 using namespace node;
@@ -15,6 +38,7 @@ static Persistent<String> uint32_symbol;
 static Persistent<String> float_symbol;
 static Persistent<String> double_symbol;
 static Persistent<String> pixel_symbol;
+
 
 Handle<Value> MakeFastBuffer(const Arguments& args) {
   HandleScope scope;
@@ -87,6 +111,7 @@ Handle<Value> MakeFastBuffer(const Arguments& args) {
   return Undefined();
 }
 
+
 void Initialize(Handle<Object> target) {
   int8_symbol   = NODE_PSYMBOL("int8");
   uint8_symbol  = NODE_PSYMBOL("uint8");
@@ -100,5 +125,6 @@ void Initialize(Handle<Object> target) {
 
   NODE_SET_METHOD(target, "makeFastBuffer", MakeFastBuffer);
 }
+
 
 NODE_MODULE(binding, Initialize);
--- test/data-view-test.js
+++ test/data-view-test.js
@@ -1,3 +1,26 @@
+/*
+** Copyright (c) 2012 The Khronos Group Inc.
+**
+** Permission is hereby granted, free of charge, to any person obtaining a
+** copy of this software and /or associated documentation files (the
+** "Materials"), to deal in the Materials without restriction, including
+** without limitation the rights to use, copy, modify, merge, publish,
+** distribute, sublicense, and/or sell copies of the Materials, and to
+** permit persons to whom the Materials are furnished to do so, subject to
+** the following conditions:
+**
+** The above copyright notice and this permission notice shall be included
+** in all copies or substantial portions of the Materials.
+**
+** THE MATERIALS ARE PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+** IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+** CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+** TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+** MATERIALS OR THE USE OR OTHER DEALINGS IN THE MATERIALS.
+*/
+
 description("Test DataView.");
 
 var intArray1 = [0, 1, 2, 3, 100, 101, 102, 103, 128, 129, 130, 131, 252, 253, 254, 255];
